688327a59c212f177287b9ee62c867b50c4cd831
Richard Perez <PEREZR27@my.erau.edu>
2020-08-04 11:16:05 -0500

    All of actions previous code now in methods

--- SpaceTrading/SpaceTrading/Actions.cs
+++ SpaceTrading/SpaceTrading/Actions.cs
@@ -4,10 +4,15 @@ using System.Text;
 
 namespace SpaceTrading
 {
-    public class Actions
+    class Actions
     {
+        public Actions()
+        {
+            
+        }
         public void startGame()
-        { // Start by asking for the user's name:
+        { 
+            // Start by asking for the user's name:
             Console.Write("What is your name?: ");
             string name = Console.ReadLine();
             Console.WriteLine($"Hello, {name}! Welcome to your story.");
@@ -33,47 +38,66 @@ namespace SpaceTrading
             }
             catch
             {
-                throw new SystemException
+                throw new SystemException;
             }
          
         }
+        public void planetChoice()
+        {
+            string planetChoice = Console.ReadLine().ToUpper();
+            if (planetChoice == "X")
+            {
+                Console.WriteLine("You've landed on planet X, the locals are hostile, Will you fight!?!");
+                Console.Write("Type your answer:");
 
-        //string planetChoice = Console.ReadLine().ToUpper();
-        //                if (planetChoice == "X")
-        //                {
-        //                    Console.WriteLine("You've landed on planet X, the locals are hostile, Will you fight!?!");
-        //                    Console.Write("Type your answer:");
+            }
+            else if (planetChoice == "Y")
+            {
+                Console.WriteLine("You've landed on planet Y, the locals are offer you a choice of three treasure chest, which one will you take!?!");
+                Console.Write("Enter a number (1-3):");
+            }
+        }
+    
+        void actionChoice()
+        {
+            string actionChoice = Console.ReadLine().ToUpper();
+            try
+            {
+                if (actionChoice == "YES")
+                {
+                    Console.WriteLine("You successfully bludgeon all the locals. You now have your payment and some extra gas.");
+                }
+                else
+                {
+                    Console.WriteLine("You were flayed and battered.");
+                }
+            }
+            catch
+            {
+                throw new SystemException;
+            }
+          
+        }
 
-        //                }
-        //                else if (planetChoice == "Y")
-        //                {
-        //                    Console.WriteLine("You've landed on planet Y, the locals are offer you a choice of three treasure chest, which one will you take!?!");
-        //                    Console.Write("Enter a number (1-3):");
-        //                }
-        //                string actionChoice = Console.ReadLine().ToUpper();
-        //string chestChoice = Console.ReadLine();
-        //                if (actionChoice == "YES")
-        //                {
-        //                    Console.WriteLine("You successfully bludgeon all the locals. You now have your payment and some extra gas.");
-        //                }
-        //                else
-        //                {
-        //                    Console.WriteLine("You were flayed and battered.");
-        //                }
+        public void chestChoice()
+        {
+            string chestChoice = Console.ReadLine();
+            switch (chestChoice)
+            {
+                case "1":
+                    Console.WriteLine("You choose the first chest. Lucky choice! There is 200 space dollars");
+                    break;
+                case "2":
+                    Console.WriteLine("You choose the second chest. A space snake bit you and you died.");
+                    break;
+                case "3":
+                    Console.WriteLine("You choose the third chest. It is empty.....");
+                    break;
+                default:
+                    break;
+            }
+        }
 
-        //                switch (chestChoice)
-        //                {
-        //                    case "1":
-        //                        Console.WriteLine("You choose the first chest. Lucky choice! There is 200 space dollars");
-        //                        break;
-        //                    case "2":
-        //                        Console.WriteLine("You choose the second chest. A space snake bit you and you died.");
-        //                        break;
-        //                    case "3":
-        //                        Console.WriteLine("You choose the third chest. It is empty.....");
-        //                        break;
-        //                    default:
-        //                        break;
-        //                }
+
     }
 }
